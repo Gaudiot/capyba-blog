f7e2d7466e7dcecfd59f33df18dcf555a651c190
Gaudiot <victor.gaudiot@gmail.com>
2023-11-19 20:38:30 -0300

    sign up route decoration

--- lib/routes/sign_up/components/sign_up_form.dart
+++ lib/routes/sign_up/components/sign_up_form.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
@@ -37,82 +38,132 @@ class _SignUpFormState extends State<SignUpForm> {
       padding: const EdgeInsets.all(15.0),
       child: FormBuilder(
         key: _formKey,
-        child: Flex(
-          direction: Axis.vertical,
-          children: [
-            FormTextField(
-              name: "username",
-              hintText: "My cool username",
-              labelText: "Username",
-              icon: FontAwesomeIcons.user, 
-              errorMessage: "Username must have between 6 and 20 characters",
-              validator: FormBuilderValidators.compose([
-                FormBuilderValidators.required(),
-                FormBuilderValidators.minLength(6),
-                FormBuilderValidators.maxLength(20),
-                (username){
-                  final RegExp usernameRegex = RegExp(r"^[a-zA-Z0-9_-_ ]{6,20}$");
-                  if(username != null && !usernameRegex.hasMatch(username)){
-                    return "Username can only contain letters and numbers";
-                  }
-                  return null;
-                }
-              ]),
-              onEditingComplete: () => FocusScope.of(context).nextFocus(),
-            ),
-            FormTextField(
-              name: "email",
-              hintText: "email@example.com",
-              labelText: "Email",
-              icon: FontAwesomeIcons.envelope, 
-              errorMessage: "Enter a valid email address",
-              validator: FormBuilderValidators.compose([
-                FormBuilderValidators.required(),
-                FormBuilderValidators.email()
-              ]),
-              onEditingComplete: () => FocusScope.of(context).nextFocus(),
-            ),
-            FormTextField(
-              name: "password",
-              hintText: "secret_password",
-              labelText: "Password",
-              icon: FontAwesomeIcons.lock,
-              isPassword: true,
-              errorMessage: "Password must contain at least 6 characters",
-              validator: FormBuilderValidators.compose([
-                FormBuilderValidators.required(),
-                FormBuilderValidators.minLength(6)
-              ]),
-              onEditingComplete: () => FocusScope.of(context).nextFocus(),
-            ),
-            FormTextField(
-              name: "repeat_password",
-              hintText: "secret_password",
-              labelText: "Repeat Password",
-              icon: FontAwesomeIcons.lock,
-              isPassword: true,
-              errorMessage: "Passwords do not match",
-              validator: (value){
-                if(value == null || value.isEmpty || _formKey.currentState!.fields['password']!.value != value){
-                  return "Passwords do not match";
-                }
-                return null;
-              },
-              onEditingComplete: _submitForm,
-            ),
-            TextButton(
-              onPressed: _submitForm, 
-              child: const Text("Cadastrar")
-            ),
-            TextButton(
-              onPressed: (){
-                context.pushNamed('signin');
-              },
-              child: const Text("Já tenho conta")
-            )
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              CachedNetworkImage(
+                imageUrl: "https://cdn.discordapp.com/attachments/1034282841766383697/1175077393606660117/capyba_signup.png?ex=6569eb1a&is=6557761a&hm=c05f04ce80588ebe1b3bc1a47702b6aff21ed2404d9adc09b0bae3644018bbc6&",
+                height: 300,
+              ),
+              const _UsernameField(),
+              const SizedBox(height: 5),
+              const _EmailField(),
+              const SizedBox(height: 5),
+              const _PasswordField(),
+              const SizedBox(height: 5),
+              _ConfirmPassword(
+                formKey: _formKey,
+                onComplete: () => _submitForm()
+              ),
+              ElevatedButton(
+                onPressed: _submitForm, 
+                child: const Text("Sign Up")
+              ),
+              const Divider(),
+              TextButton(
+                onPressed: (){
+                  context.pushNamed('signin');
+                },
+                child: const Text("Already have an account?")
+              )
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _UsernameField extends StatelessWidget {
+  const _UsernameField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FormTextField(
+      name: "username",
+      hintText: "My cool username",
+      labelText: "Username",
+      icon: FontAwesomeIcons.user, 
+      errorMessage: "Username must have between 6 and 20 characters",
+      validator: FormBuilderValidators.compose([
+        FormBuilderValidators.required(),
+        FormBuilderValidators.minLength(6),
+        FormBuilderValidators.maxLength(20),
+        (username){
+          final RegExp usernameRegex = RegExp(r"^[a-zA-Z0-9_-_ ]{6,20}$");
+          if(username != null && !usernameRegex.hasMatch(username)){
+            return "Username can only contain letters and numbers";
+          }
+          return null;
+        }
+      ]),
+      onEditingComplete: () => FocusScope.of(context).nextFocus(),
+    );
+  }
+}
+
+class _EmailField extends StatelessWidget {
+  const _EmailField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FormTextField(
+      name: "email",
+      hintText: "email@example.com",
+      labelText: "Email",
+      icon: FontAwesomeIcons.envelope, 
+      errorMessage: "Enter a valid email address",
+      validator: FormBuilderValidators.compose([
+        FormBuilderValidators.required(),
+        FormBuilderValidators.email()
+      ]),
+      onEditingComplete: () => FocusScope.of(context).nextFocus(),
+    );
+  }
+}
+
+class _PasswordField extends StatelessWidget {
+  const _PasswordField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FormTextField(
+      name: "password",
+      hintText: "secret_password",
+      labelText: "Password",
+      icon: FontAwesomeIcons.lock,
+      isPassword: true,
+      errorMessage: "Password must contain at least 6 characters",
+      validator: FormBuilderValidators.compose([
+        FormBuilderValidators.required(),
+        FormBuilderValidators.minLength(6)
+      ]),
+      onEditingComplete: () => FocusScope.of(context).nextFocus(),
+    );
+  }
+}
+
+class _ConfirmPassword extends StatelessWidget {
+  const _ConfirmPassword({super.key, required this.formKey, required this.onComplete});
+  final GlobalKey<FormBuilderState> formKey;
+  final VoidCallback onComplete;
+
+  @override
+  Widget build(BuildContext context) {
+    return FormTextField(
+      name: "repeat_password",
+      hintText: "secret_password",
+      labelText: "Repeat Password",
+      icon: FontAwesomeIcons.lock,
+      isPassword: true,
+      errorMessage: "Passwords do not match",
+      validator: (value){
+        if(value == null || value.isEmpty || formKey.currentState!.fields['password']!.value != value){
+          return "Passwords do not match";
+        }
+        return null;
+      },
+      onEditingComplete: onComplete,
     );
   }
 }
